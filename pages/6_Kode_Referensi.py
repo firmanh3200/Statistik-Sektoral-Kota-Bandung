5c17e88dc71eb79a73a50a6ee1e0c92c7d71a0f5
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2024-12-25 08:20:20 +0700

    apdet kode referensi

--- pages/6_Kode_Referensi.py
+++ pages/6_Kode_Referensi.py
@@ -36,5 +36,7 @@ if st.button("Buka Tautan"):
     url = pilihan[selected_option]
     st.markdown(f'<a href="{url}" target="_blank">Klik di sini untuk membuka Kode Referensi Kategori {selected_option}</a>', unsafe_allow_html=True)
 
+st.warning('Baca Pedoman: https://drive.google.com/file/d/1B37k_35MENcx6MQ5UWmnV-EleC-derH0/view?usp=sharing')
+
 st.divider()
 st.write('@Forum Satu Data Kota Bandung')
