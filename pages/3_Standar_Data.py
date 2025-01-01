93a1e8fdbbe15e6e5f77dbb9ab6e20fac6002a99
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2025-01-01 17:00:00 +0700

    update konten standar data

--- pages/3_Standar_Data.py
+++ pages/3_Standar_Data.py
@@ -12,30 +12,14 @@ st.subheader('STANDAR DATA STATISTIK NASIONAL')
 st.success('Berdasarkan Keputusan Kepala Badan Pusat Statistik Nomor 850 Tahun 2023')
 
 # Fungsi untuk mendapatkan data dari API dengan paginasi
-def get_all_data(base_url):
-    all_data = []
-    page = 0
-    while True:
-        url = f"{base_url}&page={page}"
-        response = requests.get(url)
-        data = response.json()
-        if not data['content']:
-            break
-        all_data.extend(data['content'])
-        page += 1
-    return all_data
+st.warning('Standar Data Statistik Nasional')
+# Embed URL in an iframe
+iframe_code = f"""
+<iframe src="https://indah.bps.go.id/standar-data-statistik-nasional" width="100%" height="600" style="border:none;"></iframe>
+"""
 
-# URL dasar API
-base_url = 'https://indah-api.bps.go.id/api/standar-data-statistik-nasional-2023?tipe=1&size=100'
+st.markdown(iframe_code, unsafe_allow_html=True)
 
-# Mendapatkan semua data
-all_data = get_all_data(base_url)
-
-# Mengonversi data menjadi DataFrame
-df = pd.DataFrame(all_data)
-df = df.sort_values(by='namaData')
-
-st.dataframe(df, hide_index=True, use_container_width=True)
 st.caption('Sumber: https://indah.bps.go.id/standar-data-statistik-nasional')
 st.divider()
 st.write('@Forum Satu Data Kota Bandung')
